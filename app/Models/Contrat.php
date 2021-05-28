b5f5e6d4a2642275fe6157cfbda5e7cf0c3a15d5
c-noblet <c.noblet.dev@gmail.com>
2021-05-28 21:34:09 +0200

    maj modèle contrat

--- app/Models/Contrat.php
+++ app/Models/Contrat.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Contrat extends Model
 {
     use HasFactory;
+
+    private $created_at;
+    private $updated_at;
+    private $vehicule_id;
+    private $conducteur_id;
+    private $employe_id;
+
+    public function getVehiculeId(string $vehicule_id)
+    {
+        return $this->vehicule_id;
+    }
+    
+    public function setVehiculeId(string $vehicule_id)
+    {
+        $this->vehicule_id = $vehicule_id;
+        return $this;
+    }
+
+    public function getConducteurId(string $conducteur_id)
+    {
+        return $this->conducteur_id;
+    }
+    
+    public function setConducteurId(string $conducteur_id)
+    {
+        $this->conducteur_id = $conducteur_id;
+        return $this;
+    }
+
+    public function getEmployeId(string $employe_id)
+    {
+        return $this->employe_id;
+    }
+    
+    public function setEmployeId(string $employe_id)
+    {
+        $this->employe_id = $employe_id;
+        return $this;
+    }
 }
