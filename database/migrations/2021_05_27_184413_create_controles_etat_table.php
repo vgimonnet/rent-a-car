859e688d4bfbc2b835fc4bd43210e1a0065f1695
VGimonnet <valentin.gimonnet18@gmail.com>
2021-06-01 22:48:31 +0200

    Correction nom table ControleEtat

--- database/migrations/2021_05_27_184413_create_controles_etat_table.php
+++ database/migrations/2021_05_27_184413_create_controles_etat_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateControleEtatsTable extends Migration
+class CreateControlesEtatTable extends Migration
 {
     /**
      * Run the migrations.
@@ -32,6 +32,6 @@ class CreateControleEtatsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('controle_etats');
+        Schema::dropIfExists('controles_etat');
     }
 }
